d4e5fc373fdfa72e799cbafa3fe6f77264b2a3f1
queenoko <quimaudongwo@gmail.com>
2020-10-13 14:23:15 +0100

    Product Component

---
+++ amazon-clone-qe/src/Header.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Header.css';
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function header() {
+    return <nav className="header">
+        {/* logo  on the left  -> image tag*/}
+        <Link to="/">
+        <img className="header__logo" 
+         src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" 
+         alt=""/>
+        </Link>
+        
+        {/* search box */}
+        <div className="header__search">
+            <input type="text" className="header__searchInput"/>
+            <SearchIcon className="header__searchIcon" />
+        </div>
+        
+        {/* three(3) links on the right */}
+        <div className="header__nav">
+            {/* 1st link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Queensly</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+            </Link>
+
+            {/* 2nd link */}
+            <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+            </Link>
+
+            {/* 3rd link */}
+            <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+            </Link>
+
+            {/* 4th link */}
+            <Link to="/checkout" className="header__link">
+                <div className="header__optionBasket">
+                    {/* Shopping basket icon */}
+                    <ShoppingBasketIcon />
+                    {/* Number of items in the basket */}
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
+            </Link>
+
+        </div>
+        {/* basket icon with number*/}
+            
+        </nav>
+}
+
+export default header
---
+++ amazon-clone-qe/src/Home.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Product from './Product';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+           <img 
+             className="home__image"
+             src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/TheBoysS2/THBY_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_1500x600_PV_en-GB._CB406828640_.jpg"
+             alt=""
+          /> 
+
+          {/* Product id. title, price. rating, image */}
+          <div className="home__row">
+              <Product 
+                id="123456775"
+                title="Comic books for children for all ages"
+                price={11.96}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/6/65/Fair_use_icon_-_Comics.svg"
+              />
+
+              <Product 
+                id="470726469188"
+                title="Washing Machine"
+                price={50.11}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/b/b3/Washing_Machine_Beko.jpg"
+              />
+          </div>
+
+          <div className="home__row">
+              <Product 
+                id="480469474728"
+                title="Wrist Watch"
+                price={7.45}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/a/ad/Stopwatch_Mode_in_Casio_Wristwatch.jpg"
+              />
+
+              <Product 
+                id="57902175199"
+                title="Speaker"
+                price={11.96}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/6/67/JBL_Flip_3_bluetooth_speaker_%28DSCF2653%29.jpg"
+              />
+
+              <Product 
+                id="6482315612"
+                title="Iphone 11 Pro"
+                price={77.81}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/9/94/IPhone_11_Pro_Midnight_Green.svg"
+              />
+          </div>
+
+          <div className="home__row">
+              <Product 
+                id="4864887194561"
+                title="Television"
+                price={110.96}
+                rating={5}
+                image="https://upload.wikimedia.org/wikipedia/commons/8/87/LG_smart_TV.png"
+              />
+
+          </div>
+
+        </div>
+    );
+}
+
+export default Home
